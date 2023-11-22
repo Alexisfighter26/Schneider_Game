8f0fb04badb72facf65a5dbed1714e5fb95110aa
Alexisfighter26 <148244499+Alexisfighter26@users.noreply.github.com>
2023-11-22 09:43:23 -0500

    Creating a Class for my Character
    Giving my Character Attributes, assigning position, velocity, and drawing.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
-import time
-import sys
-from main import utils
+import random
+import utils
+
+# import time
+# import sys
+
 
 # Initialize Pygame
 pygame.init()
@@ -38,11 +41,43 @@ while running:
     screen.blit(background, (0, 0))
 
     # Update screen based on your game.
+    pygame.display.flip()
+# (=================================================)
 
 # Character intro
-    John = player()
+W, H = 800, 447
+win = pygame.display.set_mode((W, H))
+pygame.display.set_caption('Side Scroller')
 
-    # Show screen to user.
-    pygame.display.flip()
+bg = pygame.image.load(os.path.join('assets/sprites/sus.png')).convert()
+bgX = 0
+bgX2 = bg.get_width()
 
-pygame.quit()
+clock = pygame.time.Clock()
+
+# (=====================Character Class======================)
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, velocity_x, velocity_y):
+        super().__init__()
+        self.position = (x, y)
+        self.velocity = (velocity_x, velocity_y)
+
+    def update(self):
+        self.position = (self.position[0] + self.velocity[0], self.position[1] + self.velocity[1])
+
+    def draw(self, surface):
+        pygame.draw.circle(surface, (255, 0, 0), self.position, 10)
+
+
+# (=================================================)
+john = Player(screen)
+screen.blit(john)
+
+# Show screen to user
+run = True
+while run:
+    for event in pygame.event.get():  # Loop through a list of events
+        if event.type == pygame.QUIT:  # See if the user clicks the red x
+            run = False    # End the loop
+            pygame.quit()  # Quit the game
+            quit()
