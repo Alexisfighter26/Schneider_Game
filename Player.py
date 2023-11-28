0a297731e4e1ade78692d4821cceb2570e6c98dc
Alexisfighter26 <148244499+Alexisfighter26@users.noreply.github.com>
2023-11-28 10:40:35 -0500

    Included the same as last input
    commit

--- Player.py
+++ Player.py
@@ -1,17 +1,25 @@
 import pygame
 
-class Player:
+class Player(pygame.sprite.Sprite):
     def __init__(self, x=0, y=0):
+        super().__init__() # Super init initializes the sprite
 
         # Attributes of player
         self.image = pygame.image.load('assets/sprites/sus.png').convert()
-        self.image.set_colorkey(251,251,251)
+        self.image.set_colorkey((251,251,251)) # Making this a tuple
 
         # This makes the player move at 240 px/seconds cause (px/loop)
         self.plyr_spd = 4
 
+        # Initial position
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
         # Update player position
         def update_plyr_pos(self):
             self.x += self.plyr_spd
             self.y += self.plyr_spd
+
+        # Creating my player object
+        player = Player(x=50, y=100)
