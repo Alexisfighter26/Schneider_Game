16f64fea026acf52a1614d7882b61dfcfc247636
Alexisfighter26 <148244499+Alexisfighter26@users.noreply.github.com>
2023-11-28 10:16:32 -0500

    Character plotting
    Resizing image, and updating position

--- Player.py
+++ Player.py
@@ -1,25 +1,17 @@
 import pygame
 
 class Player:
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0):
+
         # Attributes of player
-
         self.image = pygame.image.load('assets/sprites/sus.png').convert()
+        self.image.set_colorkey(251,251,251)
 
-        self.rect = self.image.get_rect()
-        self.x = x
-        self.y = y
-        self.get_rect()
-
-
-        # Setting Speed
-        self.player_x_spd = plyr_spd
-        self.player_y_spd = plyr_spd
-
-        plyr_spd = 4
+        # This makes the player move at 240 px/seconds cause (px/loop)
+        self.plyr_spd = 4
 
 
-
-        # Drawing character
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
+        # Update player position
+        def update_plyr_pos(self):
+            self.x += self.plyr_spd
+            self.y += self.plyr_spd
