0da459187427e981006a7fb249967373f1feef81
Alexisfighter26 <148244499+Alexisfighter26@users.noreply.github.com>
2023-12-03 02:32:49 -0500

    DONE?!
    I think I finished everything. I will definitely have to go back and make sure everything is oriented neatly.

--- objects.py
+++ objects.py
@@ -8,35 +8,85 @@ screen_height = 428
 class Jewel(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        # Same thing as player, initialize image, position and physics
-        self.image = pygame.image.load('assets/sprites/yellowGem.png').convert()
-        # creates an object around my character (easily able to manipulate collisions, and positioning)
+        self.image = pygame.image.load('assets/sprites/yellowGem.png').convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect = self.image.get_rect(center=(360, 400))
-        #self.set_random_position(screen_width, screen_height)
         self.points = 10
-    '''def set_random_position(self, screen_width, screen_height):
-        # Set the position of the jewel randomly within the screen boundaries
-        self.rect.x = random.randint(0, screen_width - self.rect.width)
-        self.rect.y = random.randint(0, (screen_height - 64) - (self.rect.height-128))'''
+
+    def set_random_position(self, screen_width, screen_height, platform_group):
+        valid_position = False
+        while not valid_position:
+            self.rect.x = random.randint(0, screen_width - self.rect.width)
+            self.rect.y = random.randint(0, screen_height - self.rect.height)
+
+            collisions = pygame.sprite.spritecollide(self, platform_group, False)
+            if not collisions:
+                valid_position = True
 
 class HealthItem(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        # Same thing as player, initialize image, position, and physics
-        self.image = pygame.image.load('assets/sprites/greenCrystal.png').convert()
-        # creates an object around my character (easily able to manipulate collisions, and positioning)
+        self.image = pygame.image.load('assets/sprites/greenCrystal.png').convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect = self.image.get_rect(center=(306, 400))  # Set the initial position of the player
-        '''self.set_random_position(screen_width, screen_height)'''
 
-    '''def set_random_position(self, screen_width, screen_height):
-        # Set the position of the health item randomly within the screen boundaries
-        self.rect.x = random.randint(0, (screen_width - self.rect.width))
-        self.rect.y = random.randint(0, (screen_height - self.rect.height))'''
+    def set_random_position(self, screen_width, screen_height, platform_group):
+        valid_position = False
+        while not valid_position:
+            self.rect.x = random.randint(0, screen_width - self.rect.width)
+            self.rect.y = random.randint(0, screen_height - self.rect.height)
+
+            collisions = pygame.sprite.spritecollide(self, platform_group, False)
+            if not collisions:
+                valid_position = True
 
     def interact(self, player):
         # Some logic to handle interaction with the player
         player.increase_health(20)
+
+'''class Jewel(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('assets/sprites/yellowGem.png').convert()
+        self.rect = self.image.get_rect()
+        self.points = 10
+
+    def set_random_position(self, screen_width, screen_height, platform_group):
+        valid_position = False
+        while not valid_position:
+            self.rect.x = random.randint(0, screen_width - self.rect.width)
+            self.rect.y = random.randint(0, (screen_height - 64) - (self.rect.height - 128))
+
+            collisions = pygame.sprite.spritecollide(self, platform_group, False)
+            if not collisions:
+                valid_position = True
+
+    def set_random_position(self, screen_width, screen_height):
+        # Set the position of the jewel randomly within the screen boundaries
+        self.rect.x = random.randint(0, screen_width - self.rect.width)
+        self.rect.y = random.randint(0, (screen_height) - (self.rect.height)
+
+class HealthItem(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('assets/sprites/greenCrystal.png').convert()
+        self.rect = self.image.get_rect()
+
+    def set_random_position(self, screen_width, screen_height, platform_group):
+        valid_position = False
+        while not valid_position:
+            self.rect.x = random.randint(0, screen_width - self.rect.width)
+            self.rect.y = random.randint(0, screen_height - self.rect.height)
+
+            collisions = pygame.sprite.spritecollide(self, platform_group, False)
+            if not collisions:
+                valid_position = True
+
+    def set_random_position(self, screen_width, screen_height):
+        # Set the position of the health item randomly within the screen boundaries
+        self.rect.x = random.randint(0, (screen_width - self.rect.width))
+        self.rect.y = random.randint(0, (screen_height - self.rect.height))
+
+    def interact(self, player):
+        # Some logic to handle interaction with the player
+        player.increase_health(20)'''
 
 
